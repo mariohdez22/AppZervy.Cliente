cca28ce09835e65fd0202ee7668108d08a5bfd63
MichaelTorres12 <ia.michaeltorrescallejas@gmail.com>
2024-11-26 22:45:43 -0600

    feat (build.gradle.kt nivel de APP) descomenté linea que inhabilitaba lector de google-services que daba error en tiempo de ejecución

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    //alias(libs.plugins.google.services)
+    alias(libs.plugins.google.services)
 }
 
 android {
